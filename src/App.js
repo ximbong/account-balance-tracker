925561e756f6c2fef0bb29406cd7b27d91c3d24e
Hai Truong <haitruong181096@gmail.com>
2018-06-15 15:56:45 +0300

    Add Main and 4 states.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 import Title from "./components/Title";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      incomeData: [],
+      expenseData: [],
+      totalIncome: 0,
+      totalExpense: 0
     };
   }
-  addData = data => {
-    this.setState(prevState => ({
-      data: [...this.state.data, data]
-    }));
+  addData = (data, type) => {
+    if (type === "income") {
+      this.setState({
+        incomeData: [...this.state.incomeData, data],
+        totalIncome: this.state.totalIncome + parseInt(data.amount)
+      });
+    } else {
+      this.setState({
+        expenseData: [...this.state.expenseData, data],
+        totalExpense: this.state.totalExpense + parseInt(data.amount)
+      });
+    }
   };
   render() {
-    console.log(this.state.data);
     return (
-      <div>
+      <div className="wrapper">
         <Title />
         <div className="container">
           <Header addData={this.addData} />
-          {/* <Main /> */}
+          <Main data={this.state} />
         </div>
       </div>
     );
