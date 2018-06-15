a0f365821e5ca8a1c08bdc99ee677d7bd0ef404f
Hai Truong <haitruong181096@gmail.com>
2018-06-15 16:30:55 +0300

    Add date and time.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -26,14 +26,52 @@ class Header extends Component {
       type: event.target.value
     });
   };
+  getDate = () => {
+    const date = new Date();
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1; //January is 0!
+    const yyyy = date.getFullYear();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    return dd + "/" + mm + "/" + yyyy;
+  };
+  getTime = () => {
+    const date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    if (hr < 10) {
+      hr = "0" + hr;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+
+    return hr + ":" + min + ":" + sec;
+  };
   handleClick = () => {
     const { description, amount, type } = this.state;
     const displayData = {
       description: description,
-      amount: amount
+      amount: amount,
+      date: this.getDate(),
+      time: this.getTime()
     };
     this.props.addData(displayData, type);
   };
+
   render() {
     const { description, amount, type } = this.state;
     return (
