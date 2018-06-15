cedfca8542ca54050aa16564b22c158ac55b11d4
Hai Truong <haitruong181096@gmail.com>
2018-06-15 16:32:07 +0300

    Add date and time as props in InfoRow

--- src/components/InfoGrid/index.js
+++ src/components/InfoGrid/index.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import InfoRow from "../InfoRow";
 
-const InfoGrid = ({ name, data }) => {
-  const InfoList = data.map((e, i) => (
-    <InfoRow description={e.description} amount={e.amount} key={i} />
-  ));
-  return (
-    <div className="info-box" id={name}>
-      <div className="income-type">{name}</div>
-      <div className="item-list">{InfoList}</div>
-    </div>
-  );
-};
+class InfoGrid extends PureComponent {
+  render() {
+    const { data, name } = this.props;
+    const InfoList = data.map((e, i) => (
+      <InfoRow
+        description={e.description}
+        amount={e.amount}
+        date={e.date}
+        time={e.time}
+        key={i}
+      />
+    ));
+    return (
+      <div className="info-box" id={name}>
+        <div className="income-type">{name}</div>
+        <div className="item-list">{InfoList}</div>
+      </div>
+    );
+  }
+}
 
 export default InfoGrid;
--- src/components/InfoRow/index.js
+++ src/components/InfoRow/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-const InfoRow = ({ description, amount }) => {
-  return (
-    <div className="item">
-      <div className="item-name"> {description}</div>
-      <div className="amount">
-        <span className="number">{amount}</span>
-        <span className="currency">$</span>
+class InfoRow extends PureComponent {
+  render() {
+    const { description, amount, time, date } = this.props;
+    return (
+      <div className="item">
+        <div className="item-name"> {description}</div>
+        <div className="amount">
+          <span className="number">{amount}</span>
+          <span className="currency">$</span>
+        </div>
+        <div className="time">
+          {time} {date}
+        </div>
       </div>
-      <div className="time">15/6/2018 - 19:00</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default InfoRow;
