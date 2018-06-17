f97fad987a1403c107a7032a9b51747c37232078
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-06-17 16:18:48 +0300

    Adjust title and some CSS changes

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Title = () => {
-  return <div className="title">Account Manager</div>;
+  return (
+    <div className="title">
+      <div className="name">Account Manager</div>
+      <div className="description">
+        Click on the table header to show income/expense pie chart.
+      </div>
+    </div>
+  );
 };
 
 export default Title;
