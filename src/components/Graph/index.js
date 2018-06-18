21205d0702f3cd740d80d10d5320c3f8f0b94f24
Hai Truong <haitruong181096@gmail.com>
2018-06-18 10:12:02 +0300

    Adjust the graph's height to fit the viewport height

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import Plot from "react-plotly.js";
 import "font-awesome/css/font-awesome.min.css";
 
-const Graph = ({ showGraph, graphData, graphType }) => {
-  const labels = graphData.map(e => e.description);
-  const values = graphData.map(e => e.amount);
+class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      graphHeight: 0
+    };
+  }
 
-  return (
-    <div className="main no-border-shadow">
-      <div className="close">
-        <i
-          className="fa fa-times"
-          onClick={() => {
-            showGraph(null);
+  componentDidMount() {
+    this.setState({ graphHeight: this.divRef.clientHeight - 50 });
+  }
+
+  render() {
+    const { showGraph, graphData, graphType } = this.props;
+    const labels = graphData.map(e => e.description);
+    const values = graphData.map(e => e.amount);
+
+    return (
+      <div
+        className="main no-border-shadow"
+        ref={element => (this.divRef = element)}
+      >
+        <div className="close">
+          <i
+            className="fa fa-times"
+            onClick={() => {
+              showGraph(null);
+            }}
+          />
+        </div>
+        <Plot
+          data={[
+            {
+              values: values,
+              labels: labels,
+              type: "pie",
+              name: "Dollars ($)"
+            }
+          ]}
+          layout={{
+            width: 800,
+            height: this.state.graphHeight,
+            title: `An ${graphType} pie chart`
           }}
         />
       </div>
-      <Plot
-        data={[
-          {
-            values: values,
-            labels: labels,
-            type: "pie",
-            name: "Dollars ($)"
-          }
-        ]}
-        layout={{ width: 800, height: 500, title: `An ${graphType} pie chart` }}
-      />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Graph;
